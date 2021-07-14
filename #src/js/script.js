f693a681d2bf4b175a346a019f4bed20ea6c0ab9
Daniil_Borovoy <74528634+Daniel-Borovoy@users.noreply.github.com>
2021-07-14 19:26:32 +0300

    интегрировал swiper js

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,9 +1,8 @@
-
 // Navbar *********************************************
 window.onscroll = function() {fixateNavbar()};
 let navbar = document.getElementById("nav");
-let section2 = document.getElementById("section_2");
 let section1 = document.getElementById("section_1");
+let section2 = document.getElementById("section_2");
 navbar.addEventListener("scroll", fixateNavbar);
 function fixateNavbar(){
   let offset = section1.getBoundingClientRect().height;
@@ -17,15 +16,14 @@ function fixateNavbar(){
   }
 }
 // *****************************************************
-
 let counter = document.querySelectorAll('.counter');
 let limit = 0; // Переменная, чтобы останавливать функцию, когда всё запустится.
-window.addEventListener('scroll', function(){  
+window.addEventListener("scroll", function(){  
   if( limit == counter.length ){ return; } 
   for(let i = 0; i < counter.length; i++){
     let pos = counter[i].getBoundingClientRect().top; //Позиция блока, считая сверху окна
     let win = window.innerHeight - 40; // На 40 пикселей меньше, чем высота окна
-    if( pos < win && counter[i].dataset.stop === "0" ){
+    if( pos < win && counter[i].dataset.stop === "0"){
       counter[i].dataset.stop = 1; // Останавливаем перезапуск счета в этом блоке
       let x = 0;
       limit++; // Счетчик будет запущен, увеличиваем переменную на 1
@@ -33,7 +31,7 @@ window.addEventListener('scroll', function(){
         // Раз в 60 миллисекунд будет прибавляться 50-я часть нужного числа
         x = x + Math.ceil( counter[i].dataset.to / 50 ); 
         counter[i].innerText = x + "%";
-        if( x > counter[i].dataset.to ){
+        if(x > counter[i].dataset.to){
           //Как только досчитали - стираем интервал.
           counter[i].innerText = counter[i].dataset.to + "%";
           clearInterval(int);
@@ -68,48 +66,68 @@ for (let i = 0; i < popupLinks.length; i++){
       popups[i].classList.remove("popup__open");
   });
   }
-
 // POPUPS END **************************************************
 const menu_Links = document.querySelectorAll(".menu_link[data-goto]");
 if (menu_Links.length){
   menu_Links.forEach(menu_link => {
     menu_link.addEventListener("click", onMenuLinkClick)
   });
-
-  function onMenuLinkClick(e){
-    const menuLink = e.target;
-    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-      const gotoBlock = document.querySelector(menuLink.dataset.goto);
-      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.getElementById("nav").offsetHeight;``
-      window.scrollTo({
-        top: gotoBlockValue,
-        behavior: "smooth"
-      });
-      e.preventDefault();
+function onMenuLinkClick(e){
+  const menuLink = e.target;  
+  if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+    const gotoBlock = document.querySelector(menuLink.dataset.goto);
+    const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.getElementById("nav").offsetHeight;``
+    window.scrollTo({
+      top: gotoBlockValue,
+      behavior: "smooth"
+    });
+    e.preventDefault();
     } 
-  }
+  }  
 }
-// GALLERY *********************************
-let buttonNext = document.querySelector(".next");
-let buttonBack = document.querySelector(".back");
-let gallery = document.querySelector(".gallery");
-let images = document.querySelectorAll(".gallery img");
-let valueX = 0;
-let imgNumber = 1;
-buttonNext.addEventListener("click", function (e) {
-  if (imgNumber < images.length){
-    imgNumber++;
-    valueX -= 400;
-    gallery.style.transform = `translateX(${valueX}px)`;
-  }
-})
-buttonBack.addEventListener("click", function (e) {
-  if (imgNumber !== 1){
-    imgNumber--;
-    valueX += 400;
-    gallery.style.transform = `translateX(${valueX}px)`;
-  }
-})
+// MYGALLERY *********************************
+const swiper = new Swiper('.swiper-container', {
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+// let buttonNext = document.querySelector(".next");
+// let buttonBack = document.querySelector(".back");
+// let gallery = document.querySelector(".gallery");
+// let images = document.querySelectorAll(".gallery img");
+// let valueX = 0;
+// let imgNumber = 1;
+// buttonNext.addEventListener("click", function (e) {
+//   if (imgNumber < images.length){
+//     imgNumber++;
+//     valueX -= 400;
+//     gallery.style.transform = `translateX(${valueX}px)`;
+//   }
+// })
+// buttonBack.addEventListener("click", function (e) {
+//   if (imgNumber !== 1){
+//     imgNumber--;
+//     valueX += 400;
+//     gallery.style.transform = `translateX(${valueX}px)`;
+//   }
+// })
+
+
+
+
+
+
+
+
 // GALLERY END *********************************
 
 
